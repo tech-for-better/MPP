b31b6c2daa717a138de5d3a3d7a12c029ebbb670
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-12 11:08:58 +0000

    Create reusable styled components for login and signup pages.
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

---
+++ src/components/Forms.styles.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+//  label
+//  form
+//  input
+//  button
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Label = styled.label``;
+
+export const Input = styled.input``;
+
+export const Button = styled.button``;
