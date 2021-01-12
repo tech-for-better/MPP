16e1ae36bd2955d5e93bf844579fdcd5ce8c26cd
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-12 11:30:58 +0000

    create login page with validation
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

---
+++ src/components/Login.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { logIn } from "../utils/user-management";
+
+type Profile = {
+  email: string;
+  password: string;
+};
+
+export const Login = () => {
+  const { handleSubmit, register, errors } = useForm<Profile>();
+  const onSubmit = (data: Profile) => {
+    console.log("data", data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor='email'>Email</label>
+        <input ref={register({ required: true })} id='email' name='email' type='text' />
+        {errors.email && "Enter your email"}
+      </div>
+      <div>
+        <label htmlFor='password'>Password</label>
+        <input ref={register({ required: true })} id='password' name='password' type='text' />
+        {errors.password && "Enter your password"}
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  );
+};
+
+export default Login;
