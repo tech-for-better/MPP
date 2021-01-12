1b760a3f68340ce29f87345ca1aad087041783eb
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-12 10:33:01 +0000

    modify connection.js with commonjs imports

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,12 @@
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-import firebase from "firebase/app";
+// import firebase from "firebase/app";
+
+var firebase = require("firebase/app");
 
 // Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+require("firebase/auth");
+require("firebase/firestore");
 
 // TODO: Replace the following with your app's Firebase project configuration
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
