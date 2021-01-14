95c34c3adaebc78a728b357625f1696345a4b123
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-01-14 15:35:06 +0000

    Create skeleton pages for boxing and meditation

---
+++ src/components/Meditation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PageWrapper, Slogan, Span, Button, Text, Arrow, Image, LogoImg, StyledLink } from "./Onboarding.styles";
+
+export const Homepage = () => {
+  return (
+    <PageWrapper>
+      <Slogan>
+        Throw a jab. Meditate.
+        <Span> Live a balanced life. </Span>
+      </Slogan>
+    </PageWrapper>
+  );
+};
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PageWrapper, Slogan, Span,} from "./Onboarding.styles";
+
+export const Homepage = () => {
+  return (
+    <PageWrapper>
+      <Slogan>
+        Throw a jab. Meditate.
+        <Span> Live a balanced life. </Span>
+      </Slogan>
+    </PageWrapper>
+  );
+};
