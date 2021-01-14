2c45b9092083e3dc7fa8a44ce3b4fed97b626589
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-14 18:17:26 +0000

    Styled and position navbar to the bottom of the page
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,65 @@
 import React from "react";
 import { PageWrapper, Slogan } from "./Onboarding.styles";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import progressIcon from "../assets/progress-icon.svg";
+import homeIcon from "../assets/home-icon.svg";
+import mindIcon from "../assets/mind-icon.svg";
+import bodyIcon from "../assets/body-icon.svg";
 
 const NavBar = () => {
-  return (
-      <>
-    <PageWrapper>
-      <Slogan>
-        The Navigation Bar 🧭 ✨
-      </Slogan>
-      <Navigation>
+  const pathname: string = window.location.pathname;
 
+  return (
+    <>
+      <Navigation>
+        <NavLink to='/home'>
+          <Icon src={homeIcon} alt='Clickable Home Page Navigation Icon' />
+          <span>Home</span>
+        </NavLink>
+
+        <NavLink to='/mind'>
+          <Icon src={mindIcon} alt='Clickable Mind Page Navigation Icon' />
+          <span>Mind</span>
+        </NavLink>
+
+        <NavLink to='/body'>
+          <Icon src={bodyIcon} alt='Clickable Body Page Navigation Icon' />
+          <span>Body</span>
+        </NavLink>
+
+        <NavLink to='/progress'>
+          <Icon src={progressIcon} alt='Clickable Progress Page Navigation Icon' />
+          <span>Progress</span>
+        </NavLink>
       </Navigation>
-    </PageWrapper>
+      {/* </PageWrapper> */}
     </>
   );
 };
 
-const Navigation = styled.footer``;
+const Navigation = styled.nav`
+  position: absolute;
+  display: flex;
+  background-color: #d9f0fd;
+  width: 100%;
+  height: auto;
+  justify-content: space-evenly;
+  bottom: 0;
+`;
+
+const Icon = styled.img`
+  margin-top: 15px;
+  margin-bottom: 10px;
+`;
+
+const NavLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  color: #3f4553;
+  font-weight: bold;
+  margin-bottom: 15px;
+`;
 
 export default NavBar;
