070a63c787697c306743efe48002b831bbcdf67f
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-15 10:54:48 +0000

    Create reusable logo component
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

--- src/components/MobileLogo.tsx
+++ src/components/MobileLogo.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+// import { Link } from "react-router-dom";
 import PeakIcon from "../assets/peak-icon.svg";
 
 export const MobileLogo = () => {
