97e5e2ae6aa8326c5a94adfd13fc1aa355e19cd5
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-19 16:19:57 +0000

    Add filter icons to pages
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

--- src/components/Mind.tsx
+++ src/components/Mind.tsx
@@ -3,6 +3,12 @@ import { PageWrapper, Slogan } from "./Onboarding.styles";
 import Navbar from "./NavBar";
 import { MultipleLogos } from "./MultipleLogos";
 
+import FilterButtons from "./FilterButtons";
+import calm from "../assets/Filters/calm.png";
+import focus from "../assets/Filters/focus.png";
+import connect from "../assets/Filters/connection.png";
+import switchOff from "../assets/Filters/switch-off.png";
+
 const Mind = () => {
   return (
     <>
