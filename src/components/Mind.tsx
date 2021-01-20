555431e1294a355ada893520c449a649e5b9139d
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-20 11:34:19 +0000

    Render audio and captions on mind page
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

--- src/components/Mind.tsx
+++ src/components/Mind.tsx
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 import { MultipleLogos } from "./MultipleLogos";
 import { storage, db } from "../connection";
 // import ReactAudioPlayer from 'react-audio-player';
-// import AudioPlayer from "./AudioPlayer";
+import AudioPlayer from "./AudioPlayer";
 
 import FilterButtons from "./FilterButtons";
 import calm from "../assets/Filters/calm.png";
@@ -12,54 +12,76 @@ import focus from "../assets/Filters/focus.png";
 import connect from "../assets/Filters/connection.png";
 import switchOff from "../assets/Filters/switch-off.png";
 
+// interface Provider {
+//   connected: boolean;
+//   type: string;
+// }
+
 const Mind = () => {
-  const [audios, setAudios] = React.useState({});
-  const [captions, setCaptions] = React.useState({});
+  // const [content, setContent] = React.useState({});
+  // const [audios, setAudios] = React.useState<Provider>([]);
+  const [audios, setAudios] = React.useState<Array<any>>([]);
+  const [captions, setCaptions] = React.useState<Array<any>>([]);
 
   var storageRef = storage.ref();
 
+  const result: Array<any> = [];
+  const audiosArray: Array<any> = [];
+
   React.useEffect(() => {
-    const audiosArray: string[] = [];
-    const captionsArray: string[] = [];
     storageRef
       .listAll()
       .then(function (res) {
         res.prefixes.forEach(function (folderRef) {
-          console.log(folderRef.fullPath);
           folderRef.listAll().then(function (audiofiles) {
             audiofiles.items.forEach(function (itemRef) {
               itemRef.getDownloadURL().then(url => {
-                audiosArray.push(url);
+                return audiosArray.push(url);
               });
-              setAudios(audiosArray);
               itemRef
                 .getMetadata()
                 .then(function (metadata) {
-                  captionsArray.push(metadata.customMetadata);
-                  // Metadata now contains the metadata for 'images/forest.jpg'
+                  result.push(metadata.customMetadata);
                 })
                 .catch(function (error) {
-                  // Uh-oh, an error occurred!
+                  console.error(error);
                 });
-              setCaptions(captionsArray);
+              // setCaptions(captionsArray);
             });
           });
         });
       })
       .catch(function (error) {
-        // Uh-oh, an error occurred!
+        console.error(error);
       });
+    setCaptions(result);
+    setAudios(audiosArray);
+    // console.log("captions", captionsArray);
+    console.log("audios", audios);
+    console.log("captions", captions);
   }, []);
-
-  console.log("captions", captions);
-  console.log("audios", audios);
-
+  // console.log("1", audios.length);
+  // const totalCount = audios.length;
+  // if (totalCount === undefined) return <div>Loading...</div>;
+  if (!captions) return <div>Loading...</div>;
+  console.log("capions length", captions.length);
 
   return (
     <PageWrapper>
       <MultipleLogos />
       <FilterButtons images={[calm, focus, connect, switchOff]} />
-      {/* <VideoPlayer url={video}/> */}
+      {audios.map((audio: string) => {
+        return (
+          <audio controls src={audio}>
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        );
+      })}
+      {captions.length > 0 &&
+        captions.map((caption: any) => {
+          return <p>caption[caption]</p>;
+        })}
       <NavBar />
     </PageWrapper>
   );
