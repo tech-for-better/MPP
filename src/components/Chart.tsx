5aff1794540a8f69b7ade615504efd6b12067ef5
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-20 15:18:31 +0000

    create files for progress charts

---
+++ src/components/Chart.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ResponsiveBar } from "@nivo/bar";
+// create dummy data
+// install nivo/bar
+// use it in nivo
+
+const BarChart = () => {
+  return (
+    <>
+      {/* <div class="chart">
+            <ResponsiveBar
+                data={}
+                keys={}
+                indexBy=""
+                margin={}
+                padding={}
+                colors=""
+                colorBy=""
+                defs={}
+                fill={}
+                borderColor=""
+                axisTop={}
+                axisRight={}
+                axisBottom={}
+                axisLeft={}
+                labelSkipWidth={}
+                labelSkipHeight={}
+                labelTextColor=""
+                animate={}
+                motionStiffness={}
+                motionDamping={}
+                legends={}
+            />
+        </div> */}
+    </>
+  );
+};
