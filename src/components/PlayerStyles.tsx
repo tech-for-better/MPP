0028cc3b179906840a33dfbd0b29b7b10779f055
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-23 16:56:23 +0000

    Put audio elements on top of each other in small devices
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

--- src/components/PlayerStyles.tsx
+++ src/components/PlayerStyles.tsx
@@ -5,20 +5,26 @@ interface CategoryProp {
 }
 
 export const Figure = styled.figure<CategoryProp>`
-  padding-left: 50px;
-  padding-top: 20px;
-  padding-bottom: 10px;
+  padding-top: 30px;
+  padding-bottom: 30px;
   padding-right: 100px;
   border-radius: 12px;
   background: #ffffff;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  margin-bottom: 30px;
   margin-top: 80px;
   height: 80px;
   max-height: 100%;
   max-width: 100%;
   object-fit: content;
   display: inline-flex;
+  @media screen and (max-width: 650px) {
+    display: block;
+    padding-right: 0;
+    padding-left: 0;
+    margin-left: 30px;
+    margin-right: 30px;
+    overflow-y: hidden;
+  }
 `;
 
 export const AudioTitle = styled.figcaption`
