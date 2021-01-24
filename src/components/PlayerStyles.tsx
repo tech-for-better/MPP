1e4616e8e3fec608028deb30548ee1649f9f123d
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-24 22:29:43 +0000

    Refactor code

--- src/components/PlayerStyles.tsx
+++ src/components/PlayerStyles.tsx
@@ -1,22 +1,27 @@
 import styled from "styled-components";
+import { ReactComponent as PlayIcon } from "../assets/play-icon.svg";
 
 interface CategoryProp {
   category: string;
 }
 
 export const Figure = styled.figure<CategoryProp>`
-  padding-top: 30px;
-  padding-bottom: 30px;
-  padding-right: 100px;
   border-radius: 12px;
   background: #ffffff;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   margin-top: 80px;
+  object-fit: content;
+  display: inline-flex;
+`;
+
+export const AudioFigure = styled(Figure)`
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-right: 100px;
+  /* padding: 3%; */
   height: 80px;
   max-height: 100%;
   max-width: 100%;
-  object-fit: content;
-  display: inline-flex;
   @media screen and (max-width: 650px) {
     display: block;
     padding-right: 0;
@@ -31,4 +36,9 @@ export const AudioTitle = styled.figcaption`
   font-weight: bold;
   letter-spacing: 3px;
   text-transform: uppercase;
+`;
+
+export const StyledPlayIcon = styled(PlayIcon)`
+  float: right;
+  padding-right: 0px;
 `;
