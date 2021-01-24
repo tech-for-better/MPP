c7be1aab279e843e6a35834b8d3fd3b0f10ed5fc
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-01-24 01:19:57 +0000

    Hosted latest styling version
    Co-authored-by: khadija <59174800+khadija-nur@users.noreply.github.com>

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PageWrapper, Slogan } from "./Onboarding.styles";
+import { PageWrapper } from "./Onboarding.styles";
 import NavBar from "./NavBar";
 import { MultipleLogos } from "./MultipleLogos";
 import styled from "styled-components";
@@ -16,15 +16,15 @@ const Progress = () => {
         <MultipleLogos />
         <Banner></Banner>
         <ProgressGift draggable='false' src={ProgressIcon} alt=''></ProgressGift>
-        <Slogan className="slogan"> Your Progress </Slogan>
+        <Title className="slogan"> Your Progress </Title>
         <TextWrapper> 
           <Text>
             Well done, {username}! It looks like 
             <TextSpan style={{textAlign: 'center'}}> you are on right track.</TextSpan> 
           </Text>
         </TextWrapper>
-        <NavBar />
         <Chart />
+        <NavBar />
       </PageWrapper>
     </>
   );
@@ -35,8 +35,22 @@ const Banner = styled.div`
   width: 100%;
   height: 25vh;
 `;
-const ProgressGift = styled.img``;
-const TextWrapper = styled.div``;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 20px;
+  font-weight: bolder;
+  letter-spacing: 0.04em;
+`;
+
+const ProgressGift = styled.img`
+  margin-top: -130px;
+
+`;
+const TextWrapper = styled.div`
+  margin-top: 10px;
+`;
+
 const TextSpan = styled.div``;
 
 const Text = styled.p`
