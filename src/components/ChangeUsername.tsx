128c527ef716c91511f85901e837dde610d9de3a
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-25 02:43:49 +0000

    Enable user to change username
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

---
+++ src/components/ChangeUsername.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { auth } from "../connection";
+import { Label } from "./Registration.styles";
+import { PopUpSubmit, PopUpInput } from "./PopUp";
+
+export const ChangeUsername = ({ setIsSettingsOpened, setClickedOption }: any) => {
+  const [newUsername, setNewUsername] = React.useState("");
+  var user = auth().currentUser;
+
+  const handleOnclickUpdateUsername = () => {
+    if (!user) {
+      return <p>Unable to update user at the moment</p>;
+    } else {
+      return user
+        .updateProfile({
+          displayName: newUsername,
+        })
+        .then(function () {
+          setClickedOption("");
+          setIsSettingsOpened(false);
+        })
+        .catch(function (error: any) {
+          console.error(error);
+        });
+    }
+  };
+
+  return (
+    <Confirmation>
+      <H3>Confirm</H3>
+      <form>
+        <Label htmlFor='newUsername'>
+          Enter you new username
+          <PopUpInput
+            type='text'
+            name='newUsername'
+            id='newUsername'
+            onChange={e => {
+              setNewUsername(e.target.value);
+            }}
+          />
+        </Label>
+        <PopUpSubmit onClick={handleOnclickUpdateUsername}>Update</PopUpSubmit>
+      </form>
+    </Confirmation>
+  );
+};
+
+const H3 = styled.h3`
+  color: #16212c;
+  margin-bottom: 1rem;
+`;
+
+const Confirmation = styled.div`
+  overflow: hidden;
+`;
