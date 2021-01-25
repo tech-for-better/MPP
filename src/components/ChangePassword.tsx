e899eaadb8e74d716b9078c288905554bc03af26
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-01-25 02:43:35 +0000

    Enable user to change their password
    Co-authored-by: shaya <fairyaksh@users.noreply.github.com>

---
+++ src/components/ChangePassword.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { auth } from "../connection";
+import { Input, SubmitButton, Label } from "./Registration.styles";
+import { PopUpSubmit, PopUpInput } from "./PopUp";
+
+export const ChangePassword = ({ setIsSettingsOpened, setClickedOption }: any) => {
+  const [newPassword, setNewPassword] = React.useState("");
+  var user = auth().currentUser;
+  console.log(user);
+  const handleOnclickUpdatePassword = () => {
+    if (!user) {
+      return <p>Unable to update user at the moment</p>;
+    } else {
+      return user
+        .updatePassword(newPassword)
+        .then(function () {
+          setClickedOption("");
+          setIsSettingsOpened(false);
+        })
+        .catch(function (error: any) {
+          console.error(error);
+        });
+    }
+  };
+
+  return (
+    <Confirmation>
+      <H3>Confirm</H3>
+      <form>
+        <Label htmlFor='newPassword'>
+          Enter you new password
+          <PopUpInput
+            type='text'
+            name='newPassword'
+            id='newPassword'
+            onChange={e => {
+              setNewPassword(e.target.value);
+            }}
+          />
+        </Label>
+        <PopUpSubmit onClick={handleOnclickUpdatePassword}>Update</PopUpSubmit>
+      </form>
+    </Confirmation>
+  );
+};
+
+const H3 = styled.h3`
+  color: #16212c;
+  margin-bottom: 1rem;
+`;
+
+const Confirmation = styled.div`
+  overflow: hidden;
+`;
